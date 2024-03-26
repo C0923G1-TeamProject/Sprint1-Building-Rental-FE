369448c9d99ab5123976b529fef03ad9ecb4864c
Khanhx1 <nguyentukhanh2001@gmail.com>
2024-03-26 23:06:23 +0700

    khanh update

--- sprint1-building-rental/src/components/Login/Login.js
+++ sprint1-building-rental/src/components/Login/Login.js
@@ -3,20 +3,18 @@ import '../Css/Login/Login.css'
 import '../Css/Login/js/LoginJs'
 import hide from '../Css/Login/icon-m6/hide.png'
 import view from '../Css/Login/icon-m6/view.png'
+import {Field, Form, Formik} from "formik";
+import {useState} from "react";
+
 export function Login() {
+    const [showPassword, setShowPassword] = useState(false);
 
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
-
-    const togglePasswordEye = () => {
-        let password = document.getElementById("password");
-        let eye = document.getElementById("eye");
-        if (password.type === "password") {
-            password.type = "text";
-            eye.src = view;
-        } else {
-            password.type = "password";
-            eye.src = hide;
-        }
+    const handleSubmit = (value) => {
+        console.log(value);
     }
 
 
@@ -34,42 +32,59 @@ export function Login() {
                         <div className="login-kk"></div>
                         <div className="screen">
                             <div className="screen__content">
-                                <form className="login" action="./otp.html" method="post">
-                                    <div className="login__field">
-                                        <i className="login__icon fas fa-user"><span
-                                            className="material-symbols-outlined">
+
+                                <Formik initialValues={
+                                    {
+                                        "username": "",
+                                        "password": "",
+                                        "remember-me": false
+                                    }
+                                } onSubmit={handleSubmit}>
+
+                                    <Form className="login">
+                                        <div className="login__field">
+                                            <i className="login__icon fas fa-user"><span
+                                                className="material-symbols-outlined">
 										account_circle
 									</span></i>
 
-                                        <input type="text" className="login__input" placeholder="Tài khoản"/>
-                                    </div>
-                                    <div className="login__field">
-                                        <i className="login__icon fas fa-lock"><span
-                                            className="material-symbols-outlined">
+                                            <Field name="username" type="text" className="login__input"
+                                                   placeholder="Tài khoản"/>
+                                        </div>
+                                        <div className="login__field">
+                                            <i className="login__icon fas fa-lock"><span
+                                                className="material-symbols-outlined">
 										lock
 									</span></i>
-                                        <input type="password" className="login__input" placeholder="Mật khẩu"
-                                               id="password"/>
-                                        <img src={hide} className="k-eye-icon" id="eye" onClick={togglePasswordEye}/>
-                                        <div className="k-break-wall-eye"></div>
-                                    </div>
+                                            <Field name="password"
+                                                   type={showPassword ? "text" : "password"}
+                                                   className="login__input"
+                                                   placeholder="Mật khẩu"/>
+                                            <img src={showPassword ? view : hide}
+                                                 alt={showPassword ? "show" : "hide"}
+                                                 className="k-eye-icon"
+                                                 onClick={togglePasswordVisibility}/>
+                                            <div className="k-break-wall-eye"></div>
+                                        </div>
 
-                                    <div className="checkbox-wrapper-1 k-checkbox">
-                                        <input id="example-1" className="substituted" type="checkbox"
-                                               aria-hidden="true"/>
-                                        <label htmlFor="example-1">Ghi nhớ đăng nhập</label>
-                                    </div>
+                                        <div className="checkbox-wrapper-1 k-checkbox">
+                                            <Field name="remember-me" id="example-1" className="substituted"
+                                                   type="checkbox"
+                                                   aria-hidden="true"/>
+                                            <label htmlFor="example-1">Ghi nhớ đăng nhập</label>
+                                        </div>
 
 
-                                    <button className="button login__submit" type="submit">
-                                        <span className="button__text">Đăng nhập</span>
-                                        <i className="button__icon fas fa-chevron-right"></i>
-                                    </button>
-                                    <button className="button login__submit">
-                                        <span className="button__text">Thoát</span>
-                                        <i className="button__icon fas fa-chevron-right"></i>
-                                    </button>
-                                </form>
+                                        <button className="button login__submit" type="submit">
+                                            <span className="button__text">Đăng nhập</span>
+                                            <i className="button__icon fas fa-chevron-right"></i>
+                                        </button>
+                                        <button className="button login__submit">
+                                            <span className="button__text">Thoát</span>
+                                            <i className="button__icon fas fa-chevron-right"></i>
+                                        </button>
+                                    </Form>
+                                </Formik>
 
                             </div>
                             <div className="screen__background">
