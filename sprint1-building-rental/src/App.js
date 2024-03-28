154435200f1a4746bafea6119c135918dc4410aa
Khanhx1 <nguyentukhanh2001@gmail.com>
2024-03-28 08:53:16 +0700

    k

--- sprint1-building-rental/src/App.js
+++ sprint1-building-rental/src/App.js
@@ -1,13 +1,12 @@
-
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  BrowserRouter,
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    BrowserRouter,
 } from "react-router-dom";
-import React from "react";
+import React, {useEffect} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./components/HomePage/HomePage";
 import ContactPage from "./components/HomePage/ContactPage";
@@ -16,26 +15,42 @@ import LoginPage from "./components/HomePage/LoginPage";
 import 'bootstrap/dist/js/bootstrap.bundle';
 import {Login} from "./components/Login/Login";
 import {Otp} from "./components/Login/Otp";
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/contactPage" element={<ContactPage />}></Route>
-          <Route path="/loginPage" element={<LoginPage />}></Route>
-          <Route
-            path="/introductionPage"
-            element={<IntroductionPage/>}
-          ></Route>
-          <Route path={"/login"} element={<Login/>}></Route>
-          <Route path={"/otp"} element={<Otp/>}></Route>
-        </Routes>
-      </BrowserRouter>
+import axios from "axios";
 
-    
-    </>
-  );
+function App() {
+    useEffect(() => {
+
+        const interceptor = axios.interceptors.request.use(function (config) {
+            const token = sessionStorage.getItem("token");
+            config.headers.Authorization = `Bearer ${token}`;
+            return config;
+        });
+
+        return () => {
+            axios.interceptors.request.eject(interceptor);
+        };
+    }, []);
+
+
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage/>}></Route>
+                    <Route path="/contactPage" element={<ContactPage/>}></Route>
+                    <Route path="/loginPage" element={<LoginPage/>}></Route>
+                    <Route
+                        path="/introductionPage"
+                        element={<IntroductionPage/>}
+                    ></Route>
+                    <Route path={"/login"} element={<Login/>}></Route>
+                    <Route path={"/otp"} element={<Otp/>}></Route>
+                </Routes>
+            </BrowserRouter>
+
+
+        </>
+    );
 }
 
 export default App;
