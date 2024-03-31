e8ca0feb173c2334276426b75a2d3693b97f4a40
Khanhx1 <nguyentukhanh2001@gmail.com>
2024-04-01 00:25:00 +0700

    ko

---
+++ sprint1-building-rental/src/components/Login/ModalLogin.js
@@ -0,0 +1,27 @@
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import {Link} from "react-router-dom";
+
+export const ModalLogin = ({show}) => {
+    const handleSubmit = async () => {
+
+    }
+
+    return (
+        <>
+            <Modal show={show}>
+                <Modal.Header>
+                    Đăng nhập thành công
+                </Modal.Header>
+                <Modal.Body>
+                    <Link to={"/LoginPage"} className="btn btn-warning mx-3">Đăng nhập vào hệ thống</Link>
+                    <Link to={"/information"} className="btn btn-warning mx-3">Thông tin cá nhân</Link>
+                </Modal.Body>
+                <Modal.Footer>
+
+                </Modal.Footer>
+            </Modal>
+
+        </>
+    )
+}
