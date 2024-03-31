379dc7a7412f062bbdbd91f45fa15c5d8ea82128
vanhieu272 <93814554+vanhieu272@users.noreply.github.com>
2024-03-31 15:25:39 +0700

    update FE

--- sprint1-building-rental/src/components/Premises/ListPremises.js
+++ sprint1-building-rental/src/components/Premises/ListPremises.js
@@ -16,6 +16,8 @@ export default function ListPremises() {
     const [premisesName, setPremisesName] = useState("");
     const [totalPages, setTotalPages] = useState(0);
     const [currentPage, setCurrentPage] = useState(0);
+    const [listFloor, setListFloor] = useState([]);
+    const [listType, setListType] = useState([]);
 
     useEffect(() => {
         const fetchApi = async () => {
@@ -50,7 +52,6 @@ export default function ListPremises() {
     }
 
     const submitSearch = async () => {
-        console.log(2)
         try {
 
             let res = await service.getAllPremises(floor, code, area, premisesName, 0).then();
@@ -64,17 +65,32 @@ export default function ListPremises() {
     }
 
 
-    // const handlePageClick = async (event) => {
-    //     try {
-    //         const pageNumber = event.selected;
-    //         setCurrentPage(pageNumber);
-    //         const result = await service.getAllPremises(floor, code, area, premisesName);
-    //         setPremisesList(result.content);
-    //         setTotalPages(result.totalPages);
-    //     } catch (e) {
-    //         console.log(e);
-    //     }
-    // }
+    const findAllFloor = async () => {
+        try {
+            const res = await service.getListFloor();
+            setListFloor(res);
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        findAllFloor();
+    });
+
+
+    const findAllType = async () => {
+        try {
+            const res = await service.getListType();
+            setListType(res);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        findAllType();
+    });
 
     const handlePageClick = async (event) => {
         try {
@@ -102,9 +118,9 @@ export default function ListPremises() {
     }, [currentPage]);
 
     const VND = new Intl.NumberFormat('vi-VN', {
-        style: 'currency',
-        currency: 'VND',
-        currencyDisplay: 'code', // Sử dụng mã tiền tệ
+        style: 'decimal', // Sử dụng kiểu số thập phân
+        minimumFractionDigits: 0, // Số lượng số thập phân tối thiểu là 0
+        maximumFractionDigits: 0, // Số lượng số thập phân tối đa cũng là 0
     });
 
     /*Giao diện*/
@@ -117,91 +133,100 @@ export default function ListPremises() {
                     <div className="col-1"></div>
                     <div className="col-10">
 
-                            <div className="input-group">
+                        <div className="input-group">
 
-                                <select className="form-control rounded-1 me-2"
-                                        onChange={(event) => handleFloor(event.target.value)}>
-                                    <option value="">Chọn tầngㅤㅤㅤㅤㅤㅤ▼</option>
-                                    <option value="1">Tầng 1</option>
-                                    <option value="2">Tầng 2</option>
-                                    <option value="3">Tầng 3</option>
-                                    <option value="4">Tầng 4</option>
-                                    <option value="5">Tầng 5</option>
-                                    <option value="6">Tầng 6</option>
-                                    <option value="7">Tầng 7</option>
-                                    <option value="8">Tầng 8</option>
-                                    <option value="9">Tầng 9</option>
-                                </select>
+                            <select className="form-control rounded-1 me-2"
+                                    onChange={(event) => handleFloor(event.target.value)}>
+                                <option value="">Chọn tầngㅤㅤㅤㅤㅤㅤㅤ▼</option>
+                                {
+                                    listFloor.map((item) => (
+                                        <option key={item.index} value={item}>{item}</option>
+                                    ))
+                                }
+                            </select>
 
-                                <input onChange={(event) => handleCode(event.target.value)} type="text"
-                                       className="form-control rounded-1 me-2" name="name-search" size="25"
-                                       placeholder="Mã mặt bằng"/>
+                            <select className="form-control rounded-1 me-2"
+                                    onChange={(event) => handlePremisesName(event.target.value)}>
+                                <option value="">Loại mặt bằngㅤㅤㅤㅤㅤㅤ▼</option>
+                                {
+                                    listType.map((item) => (
+                                        <option key={item.index} value={item.name}>{item.name}</option>
+                                    ))
+                                }
+                            </select>
+
+                            <input onChange={(event) => handleCode(event.target.value)} type="text"
+                                   className="form-control rounded-1 me-2" name="name-search" size="25"
+                                   placeholder="Mã mặt bằng"/>
 
 
-                                <input onChange={(event) => handleArea(event.target.value)} type="text"
-                                       className="form-control rounded-1 me-2" name="name-search" size="25"
-                                       placeholder="Diện tích tối đa"/>
+                            <input onChange={(event) => handleArea(event.target.value)} type="text"
+                                   className="form-control rounded-1 me-2" name="name-search" size="25"
+                                   placeholder="Diện tích tối đa"/>
 
 
-                                <input onChange={(event) => handlePremisesName(event.target.value)} type="text"
-                                       className="form-control rounded-1 me-2" name="name-search" size="25"
-                                       placeholder="Loại mặt bằng"/>
-
-                                <div>
-                                    <button onClick={() => submitSearch()} type="button" className="btn btn-in-list">Tìm
-                                        kiếm
-                                    </button>
-                                </div>
+                            <div>
+                                <button onClick={() => submitSearch()} type="button" className="btn btn-in-list">Tìm
+                                    kiếm
+                                </button>
                             </div>
+                        </div>
 
                     </div>
 
                     <div className="col-1"></div>
                 </div>
-                <div className="row my-3">
-                    <table className="table table-striped">
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Mã mặt bằng</th>
-                            <th>Loại mặt bằng</th>
-                            <th>Diện tích (m²)</th>
-                            <th>Trạng thái</th>
-                            <th>Giá bán</th>
-                            <th>Phí quản lý</th>
-                            <th className="text-center">Tuỳ chọn</th>
-
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {premisesList ? (
-                            premisesList.map((premises, index) => (
-                                    <tr key={premises.id}>
-                                        <td>{(currentPage - 1) * 3 + (index + 1) + 3}</td>
-                                        <td>{premises.code}</td>
-                                        <td>{premises.typePremises.name}</td>
-                                        <td>{premises.area}</td>
-                                        <td>{premises.premisesStatus.name}</td>
-                                        <td>{VND.format(premises.price)}</td>
-                                        <td>{VND.format(premises.cost)}</td>
-                                        <td className="text-center"><Link to={`/update-premises/${premises.id}`} className="custom-link-style">
-                                            <button className="btn btn-feature">Sửa</button>
-                                        </Link></td>
-
-                                    </tr>
-
-                                )
-                            )
-                        ) : (
+                <div id="tbl-custom" className="row my-3 table-responsive">
+                    <div className="col-1"></div>
+                    <div className="col-10">
+                        <table className="table table-striped">
+                            <thead>
                             <tr>
-                                <td colSpan={9}>
-                                    Không tìm thấy dữ liệu
-                                </td>
+                                <th>#</th>
+                                <th>Mã mặt bằng</th>
+                                <th>Loại mặt bằng</th>
+                                <th>Diện tích (m²)</th>
+                                <th>Tầng</th>
+                                <th>Trạng thái</th>
+                                <th>Giá bán (VND)</th>
+                                <th>Phí quản lý (VND)</th>
+                                <th className="text-center">Tuỳ chọn</th>
+
                             </tr>
-                        )
-                        }
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            {premisesList ? (
+                                premisesList.map((premises, index) => (
+                                        <tr key={premises.id}>
+                                            <td>{(index + 1)}</td>
+                                            <td>{premises.code}</td>
+                                            <td>{premises.typePremises.name}</td>
+                                            <td>{premises.area}</td>
+                                            <td>{premises.floor}</td>
+                                            <td>{premises.premisesStatus.name}</td>
+                                            <td>{VND.format(premises.price)}</td>
+                                            <td>{VND.format(premises.cost)}</td>
+                                            <td className="text-center"><Link to={`/update-premises/${premises.id}`}
+                                                                              className="custom-link-style">
+                                                <button className="btn btn-feature">Sửa</button>
+                                            </Link></td>
+
+                                        </tr>
+
+                                    )
+                                )
+                            ) : (
+                                <tr>
+                                    <td colSpan={9}>
+                                        Không tìm thấy dữ liệu
+                                    </td>
+                                </tr>
+                            )
+                            }
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="col-1>"></div>
                 </div>
                 <div className="row pb-5">
                     {premisesList ? (
@@ -227,7 +252,7 @@ export default function ListPremises() {
                                 activeClassName="active"
                             />
                         </div>
-                    ):(<div></div>)
+                    ) : (<div></div>)
                     }
                 </div>
             </div>
