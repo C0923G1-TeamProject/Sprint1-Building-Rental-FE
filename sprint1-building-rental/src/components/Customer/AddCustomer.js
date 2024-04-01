d38ba6ca8fd25c2749e9856f0782bc051c4bcdde
quocgiau <quocgiau61s@gmail.com>
2024-04-01 10:27:52 +0700

    giau

--- sprint1-building-rental/src/components/Customer/AddCustomer.js
+++ sprint1-building-rental/src/components/Customer/AddCustomer.js
@@ -2,20 +2,24 @@ import "./CustomerAdd.css"
 import {Link, useNavigate} from "react-router-dom";
 import * as Yup from "yup";
 import * as CustomerService from "../../service/CustomerService/CustomerService";
-import UploadImage from "./UploadImage"
 import {toast} from "react-toastify";
 import Helmet from "react-helmet";
 import HeaderAdmin from "../Header/HeaderAdmin";
 import {ErrorMessage, Field, Form, Formik} from "formik";
 import Footer from "../Footer/Footer";
-import {useState} from "react";
+import React, {useState} from "react";
+import storage from "../PersonalInsormation/firebase/firebaseConfig";
+import {getDownloadURL, ref, uploadBytesResumable} from "firebase/storage";
 
 function AddCustomer() {
     const navigation = useNavigate();
-    const [imageUrl, setImageUrl] = useState("");
     const [errorMessageEmail, setErrorMessageEmail] = useState("");
     const [errorMessageCard, setErrorMessageCard] = useState("");
     const [errorMessagePhone, setErrorMessagePhone] = useState("");
+    const [preview, setPreview] = useState();
+    const [file, setFile] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+    const [percent, setPercent] = useState(0);
     const validation = {
         name: Yup.string().matches(/^[a-zA-ZÀ-ỹ\s]*$/, "Tên không đúng định dạng VD: Nguyễn Văn An").required("Vui lòng nhập tên"),
         card: Yup.string().matches(/^\d{12}$/, "Số căn cước cần chính xác 12 ký tự số").required("Vui lòng nhập căn cước công dân"),
@@ -39,6 +43,7 @@ function AddCustomer() {
     const handleSubmit = async (values) => {
         try {
             // Gán giá trị URL của ảnh từ Firebase vào trường img của values
+            handleUpload()
             values.img = imageUrl;
             let rs = await CustomerService.createCustomer(values);
             switch (rs) {
@@ -69,6 +74,47 @@ function AddCustomer() {
             console.log(e);
         }
     }
+
+    function handleChange(event) {
+        setFile(event.target.files[0]);
+        setPreview(event.target.files[0]);
+    }
+
+    const handleUpload = () => {
+        if (!file) {
+            alert("Vui lòng tải ảnh lên trước!");
+        }
+
+        const storageRef = ref(storage, `/files/${file.name}`);
+
+        // progress can be paused and resumed. It also exposes progress updates.
+        // Receives the storage reference and the file to upload.
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+            "state_changed",
+            (snapshot) => {
+                const percent = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                );
+
+                // update progress
+                setPercent(percent);
+            },
+            (err) => console.log(err),
+            () => {
+                // download url
+                getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+
+                    setImageUrl(url);
+                });
+            }
+        );
+    };
+    const renderImage = () => {
+        if (preview) return URL.createObjectURL(preview);
+        return "https://cdn.sforum.vn/sforum/wp-content/uploads/2023/10/avatar-trang-4.jpg";
+    };
     return (
         <>
             <Helmet>
@@ -77,10 +123,10 @@ function AddCustomer() {
             <div>
                 <HeaderAdmin/>
                 <div className="d-flex justify-content-center row ">
-                    <h2 className="col-12 d-flex justify-content-center cus-title-add mt-5">Tạo mới khách hàng</h2>
+                    <h1 className="text-center">THÊM MỚI KHÁCH HÀNG</h1>
                 </div>
                 <div className="d-flex justify-content-center row mb-5">
-                    <div className="col-6">
+                    <div className="col-9">
                         <Formik initialValues={{
                             "name": "",
                             "card": "",
@@ -93,79 +139,104 @@ function AddCustomer() {
                             "img": ""
                         }} onSubmit={handleSubmit} validationSchema={Yup.object(validation)}>
                             <Form>
-                                <div className="form-group cus-group">
-                                    <label htmlFor="name" className="form-label cus-label">Tên khách hàng<span
-                                        className="cus-col">*</span>:</label>
-                                    <Field name="name" type="text" className="form-control cus-input" id="name"/>
-                                    <ErrorMessage name="name" component="span" className="k-span"></ErrorMessage>
-                                </div>
-                                <div className="form-group cus-group">
-                                    <label className="form-label cus-label">Hình ảnh:</label>
-                                    <UploadImage setImage={setImageUrl} className="form-control cus-input"/>
-                                    {imageUrl && (<img src={imageUrl} alt="Uploaded" style={{maxWidth: "200px"}}/>)}
-                                </div>
-                                <div className="form-group cus-group">
-                                    <label htmlFor="date" className="form-label cus-label">Ngày sinh<span
-                                        className="cus-col">*</span>:</label>
-                                    <Field name="date" type="date" className="form-control cus-input" id="date"/>
-                                    <ErrorMessage name="date" component="span" className="k-span"></ErrorMessage>
-                                </div>
-                                <div className="form-group cus-group">
-                                    <label htmlFor="card" className="form-label cus-label">CCCD<span
-                                        className="cus-col">*</span>:</label>
-                                    <Field name="card" type="text" className="form-control cus-input" id="card"/>
-                                    <ErrorMessage name="card" component="span" className="k-span"></ErrorMessage>
-                                    <div>
-                                        {errorMessageCard && (
-                                            <span className="error-message">{errorMessageCard}</span>
-                                        )}
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="form-group cus-group">
+                                            <label htmlFor="name" className="form-label cus-label">Tên khách hàng<span
+                                                className="cus-col">*</span>:</label>
+                                            <Field name="name" type="text" className="form-control cus-input"
+                                                   id="name"/>
+                                            <ErrorMessage name="name" component="span"
+                                                          className="k-span"></ErrorMessage>
+                                        </div>
+                                        <div className="form-group cus-group">
+                                            <label htmlFor="date" className="form-label cus-label">Ngày sinh<span
+                                                className="cus-col">*</span>:</label>
+                                            <Field name="date" type="date" className="form-control cus-input"
+                                                   id="date"/>
+                                            <ErrorMessage name="date" component="span"
+                                                          className="k-span"></ErrorMessage>
+                                        </div>
+                                        <div className="form-group cus-group">
+                                            <label htmlFor="email" className="form-label cus-label">Email<span
+                                                className="cus-col">*</span>:</label>
+                                            <Field name="email" type="email" className="form-control cus-input"
+                                                   id="email"/>
+                                            <div>
+                                                {errorMessageEmail && (
+                                                    <span className="error-message">{errorMessageEmail}</span>
+                                                )}
+                                            </div>
+                                            <ErrorMessage name="email" component="span"
+                                                          className="k-span"></ErrorMessage>
+                                        </div>
+                                        <div className="form-group cus-group">
+                                            <label htmlFor="address" className="form-label cus-label">Địa chỉ<span
+                                                className="cus-col">*</span>:</label>
+                                            <Field name="address" type="text" className="form-control cus-input"
+                                                   id="address"/>
+                                            <ErrorMessage name="address" component="span"
+                                                          className="k-span"></ErrorMessage>
+                                        </div>
+                                        <div className="form-group cus-group">
+                                            <label htmlFor="card" className="form-label cus-label">CCCD<span
+                                                className="cus-col">*</span>:</label>
+                                            <Field name="card" type="text" className="form-control cus-input"
+                                                   id="card"/>
+                                            <ErrorMessage name="card" component="span"
+                                                          className="k-span"></ErrorMessage>
+                                            <div>
+                                                {errorMessageCard && (
+                                                    <span className="error-message">{errorMessageCard}</span>
+                                                )}
+                                            </div>
+                                        </div>
+                                        <div className="form-group cus-group">
+                                            <label htmlFor="phone" className="form-label cus-label">Số điện thoại<span
+                                                className="cus-col">*</span>:</label>
+                                            <Field name="phoneNumber" type="text" className="form-control cus-input"
+                                                   id="phone"/>
+                                            <ErrorMessage name="phoneNumber" component="span"
+                                                          className="k-span"></ErrorMessage>
+                                            <div>
+                                                {errorMessagePhone && (
+                                                    <span className="error-message">{errorMessagePhone}</span>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="form-group cus-group">
+                                            <label className="form-label cus-label">Hình ảnh:</label>
+                                            <input
+                                                type="file"
+                                                className="form-control cus-input" style={{marginBottom:"10px"}}
+                                                onChange={handleChange}
+                                                accept="image/*"
+                                            />
+                                            <img src={renderImage()} alt="Ảnh khách hàng"
+                                                 style={{maxWidth: "240px", maxHeight: "280px", minHeight: "280px"}}/>
+                                        </div>
+                                        <div className="form-group cus-group">
+                                            <label htmlFor="url" className="form-label cus-label">Website<span
+                                                className="cus-col">*</span>:</label>
+                                            <Field name="url" type="text" className="form-control cus-input" id="url"/>
+                                            <ErrorMessage name="url" component="span" className="k-span"></ErrorMessage>
+                                        </div>
+                                        <div className="form-group cus-group">
+                                            <label htmlFor="company" className="form-label cus-label">Tên công ty<span
+                                                className="cus-col">*</span>:</label>
+                                            <Field name="company" type="text" className="form-control cus-input"
+                                                   id="company"/>
+                                            <ErrorMessage name="company" component="span"
+                                                          className="k-span"></ErrorMessage>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="form-group cus-group">
-                                    <label htmlFor="email" className="form-label cus-label">Email<span
-                                        className="cus-col">*</span>:</label>
-                                    <Field name="email" type="email" className="form-control cus-input" id="email"/>
-                                    <div>
-                                        {errorMessageEmail && (
-                                            <span className="error-message">{errorMessageEmail}</span>
-                                        )}
-                                    </div>
-                                    <ErrorMessage name="email" component="span" className="k-span"></ErrorMessage>
-                                </div>
-                                <div className="form-group cus-group">
-                                    <label htmlFor="phone" className="form-label cus-label">Số điện thoại<span
-                                        className="cus-col">*</span>:</label>
-                                    <Field name="phoneNumber" type="text" className="form-control cus-input"
-                                           id="phone"/>
-                                    <ErrorMessage name="phoneNumber" component="span" className="k-span"></ErrorMessage>
-                                    <div>
-                                        {errorMessagePhone && (
-                                            <span className="error-message">{errorMessagePhone}</span>
-                                        )}
-                                    </div>
-                                </div>
-                                <div className="form-group cus-group">
-                                    <label htmlFor="address" className="form-label cus-label">Địa chỉ<span
-                                        className="cus-col">*</span>:</label>
-                                    <Field name="address" type="text" className="form-control cus-input" id="address"/>
-                                    <ErrorMessage name="address" component="span" className="k-span"></ErrorMessage>
-                                </div>
-                                <div className="form-group cus-group">
-                                    <label htmlFor="url" className="form-label cus-label">Website<span
-                                        className="cus-col">*</span>:</label>
-                                    <Field name="url" type="text" className="form-control cus-input" id="url"/>
-                                    <ErrorMessage name="url" component="span" className="k-span"></ErrorMessage>
-                                </div>
-                                <div className="form-group cus-group">
-                                    <label htmlFor="company" className="form-label cus-label">Tên công ty<span
-                                        className="cus-col">*</span>:</label>
-                                    <Field name="company" type="text" className="form-control cus-input" id="company"/>
-                                    <ErrorMessage name="company" component="span" className="k-span"></ErrorMessage>
-                                </div>
-                                <div className="btn-container custom-btn-1">
-                                    <button className="btn cus">
+                                <div className="btn-container ">
+                                    <div className="btn cus custom-btn-1">
                                         <Link to={"/customer"} className="cus-display-add">Huỷ thêm mới</Link>
-                                    </button>
+                                    </div>
                                     <button className="btn cus custom-btn" type="submit">Thêm khách hàng</button>
                                 </div>
 
